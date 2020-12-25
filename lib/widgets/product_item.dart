580edf7f499b2318b350a8792e83902f8f7684c5
Andrea Guffi <andreaguffi92@gmail.com>
2020-12-25 19:15:20 +0100

    ok

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,18 +1,19 @@
+import 'package:ecommerce_app/providers/products.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../screens/product_detail_screen.dart';
 import '../providers/product.dart';
 
 class ProductItem extends StatelessWidget {
-  //final String id;
-  //final String title;
-  //final String imageUrl;
+  // final String id;
+  // final String title;
+  // final String imageUrl;
 
-  //ProductItem(this.id, this.title, this.imageUrl);
+  // ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
-    final product = Provider.of<Product>(context);
+    final product = Provider.of<Product>(context, listen: false);
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
@@ -30,14 +31,16 @@ class ProductItem extends StatelessWidget {
         ),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
-          leading: IconButton(
-            icon: Icon(product.isFavorite
-                ? Icons.favorite
-                : Icons.favorite_border), // check aggiornare ui favorite icon
-            color: Theme.of(context).accentColor,
-            onPressed: () {
-              product.toggleFavoriteStatus();
-            },
+          leading: Consumer<Product>(
+            builder: (ctx, product, child) => IconButton(
+              icon: Icon(
+                product.isFavorite ? Icons.favorite : Icons.favorite_border,
+              ),
+              color: Theme.of(context).accentColor,
+              onPressed: () {
+                product.toggleFavoriteStatus();
+              },
+            ),
           ),
           title: Text(
             product.title,
