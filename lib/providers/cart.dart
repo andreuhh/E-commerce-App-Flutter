7f7b3a1fa9a10bdfd12ae08d83b2882ce32c7104
Andrea Guffi <andreaguffi92@gmail.com>
2021-02-21 16:33:05 +0100

    implemented SnackBar

--- lib/providers/cart.dart
+++ lib/providers/cart.dart
@@ -70,6 +70,25 @@ class Cart with ChangeNotifier {
     notifyListeners();
   }
 
+  void removeSingleItem(String productId) {
+    if (!_items.containsKey(productId)) {
+      return;
+    }
+    if (_items[productId].quantity > 1) {
+      _items.update(
+          productId,
+          (existingCartItem) => CartItem(
+                id: existingCartItem.id,
+                title: existingCartItem.title,
+                quantity: -1,
+                price: existingCartItem.price,
+              ));
+    } else {
+      _items.remove(productId);
+    }
+    notifyListeners();
+  }
+
   // delete the elements in the chart
   void clear() {
     _items = {};
