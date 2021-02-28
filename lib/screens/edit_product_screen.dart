0ac2bc2abf13b60ceb12761282f18a3849de004c
Andrea Guffi <andreaguffi92@gmail.com>
2021-02-28 18:20:49 +0100

    prod validation

--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -46,6 +46,10 @@ class _EditProductScreenState extends State<EditProductScreen> {
 
   void _saveForm() {
     // save() è un metodo di Flutter per salvare i form
+    final isValid = _form.currentState.validate();
+    if (isValid) {
+      return;
+    }
     _form.currentState.save();
     print(_editedProduct.title);
     print(_editedProduct.description);
@@ -71,10 +75,18 @@ class _EditProductScreenState extends State<EditProductScreen> {
           child: ListView(
             children: <Widget>[
               TextFormField(
-                decoration: InputDecoration(labelText: 'Title'),
+                decoration: InputDecoration(
+                  labelText: 'Title',
+                ),
                 textInputAction: TextInputAction.next,
                 onFieldSubmitted: (_) {
                   FocusScope.of(context).requestFocus(_priceFocusNode);
+                },
+                validator: (value) {
+                  if (value.isEmpty) {
+                    return 'Please provide a value';
+                  }
+                  return null;
                 },
                 onSaved: (value) {
                   _editedProduct = Product(
