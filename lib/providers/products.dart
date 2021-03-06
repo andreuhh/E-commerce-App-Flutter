75ec204acd57017ed26fab16df3a7703392a8a0e
Andrea Guffi <andreaguffi92@gmail.com>
2021-03-06 21:47:00 +0100

    handling error

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -97,6 +97,9 @@ class Products with ChangeNotifier {
       _items.add(newProduct);
       // _items.insert(0, newProduct); // at start of the list
       notifyListeners();
+    }).catchError((error) {
+      print(error);
+      throw error;
     });
   }
 
--- lib/screens/edit_product_screen.dart
+++ lib/screens/edit_product_screen.dart
@@ -107,7 +107,22 @@ class _EditProductScreenState extends State<EditProductScreen> {
     } else {
       Provider.of<Products>(context, listen: false)
           .addProduct(_editedProduct)
-          .then((_) {
+          .catchError((error) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text('An error occurred'),
+                  content: Text('Something went wrong'),
+                  actions: <Widget>[
+                    FlatButton(
+                      child: Text('Okay'),
+                      onPressed: () {
+                        Navigator.of(ctx).pop(); // close page
+                      },
+                    )
+                  ],
+                ));
+      }).then((_) {
         setState(() {
           _isLoading = false;
         });
