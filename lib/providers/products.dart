59532277b7bd0627d0008a9162331a58a72fcce8
Andrea Guffi <andreaguffi92@gmail.com>
2021-03-07 16:22:14 +0100

    edit product okk

--- lib/providers/products.dart
+++ lib/providers/products.dart
@@ -126,9 +126,18 @@ class Products with ChangeNotifier {
     }
   }
 
-  void updateProduct(String id, Product newProduct) {
+  Future<void> updateProduct(String id, Product newProduct) async {
     final prodIndex = _items.indexWhere((prod) => prod.id == id);
     if (prodIndex >= 0) {
+      final url =
+          'https://flutter-e-commerce-8ea1f-default-rtdb.firebaseio.com/products/$id.json';
+      await http.patch(url,
+          body: json.encode({
+            'title': newProduct.title,
+            'description': newProduct.description,
+            'imageUrl': newProduct.imageUrl,
+            'price': newProduct.price
+          }));
       _items[prodIndex] = newProduct;
       notifyListeners();
     } else {
