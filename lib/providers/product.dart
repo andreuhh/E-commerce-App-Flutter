894341c9fb79e63e5fa7d4779ed8197301d54644
Andrea Guffi <andreaguffi92@gmail.com>
2021-03-10 20:58:59 +0100

    fav product

--- lib/providers/product.dart
+++ lib/providers/product.dart
@@ -1,4 +1,6 @@
+import 'dart:convert'; //offers tools to convert data
 import 'package:flutter/foundation.dart';
+import 'package:http/http.dart' as http;
 
 class Product with ChangeNotifier {
   final String id;
@@ -17,8 +19,26 @@ class Product with ChangeNotifier {
     this.isFavorite = false,
   });
 
-  void toggleFavoriteStatus() {
+  void _setFavValue(bool newValue) {
+    isFavorite = newValue;
+    notifyListeners();
+  }
+
+  Future<void> toggleFavoriteStatus() async {
+    final oldStatus = !isFavorite;
     isFavorite = !isFavorite;
     notifyListeners();
+    final url =
+        'https://flutter-e-commerce-8ea1f-default-rtdb.firebaseio.com/products/$id.json';
+    try {
+      final response =
+          await http.patch(url, body: json.encode({'isFavorite': isFavorite}));
+
+      if (response.statusCode >= 400) {
+        _setFavValue(oldStatus);
+      }
+    } catch (error) {
+      _setFavValue(oldStatus);
+    }
   }
 }
