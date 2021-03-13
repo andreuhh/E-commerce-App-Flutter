c23e3eac201fb50d09d3264df38700f14811fa9a
Andrea Guffi <andreaguffi92@gmail.com>
2021-03-13 09:21:35 +0100

    login

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -7,9 +7,10 @@ class Auth with ChangeNotifier {
   DateTime _expiryDate;
   String _userId;
 
-  Future<void> signup(String email, String password) async {
-    const url =
-        'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDON62440TlETUc-A3oq_NFZDWM4JV1xkw';
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=AIzaSyDON62440TlETUc-A3oq_NFZDWM4JV1xkw';
 
     final response = await http.post(
       url,
@@ -21,8 +22,14 @@ class Auth with ChangeNotifier {
         },
       ),
     );
-    print(
-      json.decode(response.body),
-    );
+    print(json.decode(response.body));
+  }
+
+  Future<void> signup(String email, String password) async {
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> login(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
   }
 }
