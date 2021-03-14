32fdbbfd5a62e363ebfd72d57287c537ebc5d4a8
Andrea Guffi <andreaguffi92@gmail.com>
2021-03-14 13:59:29 +0100

    hero animation

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -27,10 +27,14 @@ class ProductItem extends StatelessWidget {
                 arguments: product.id,
               );
             },
-            child: FadeInImage(
-              placeholder: AssetImage('assets/images/product-placeholder.png'),
-              image: NetworkImage(product.imageUrl),
-              fit: BoxFit.cover,
+            child: Hero(
+              tag: product.id,
+              child: FadeInImage(
+                placeholder:
+                    AssetImage('assets/images/product-placeholder.png'),
+                image: NetworkImage(product.imageUrl),
+                fit: BoxFit.cover,
+              ),
             )),
         footer: GridTileBar(
           backgroundColor: Colors.black87,
